1280f6ee9d470c9772f25abeca04b5d63a8a4dd8
Yaroslav Kalashnikov <yekalashnikov@gmail.com>
2024-08-11 20:18:10 +0300

    feat: update Home page component

--- client/notes-application/src/pages/home/Home.jsx
+++ client/notes-application/src/pages/home/Home.jsx
@@ -47,7 +47,13 @@ const Home = () => {
         contentLabel=""
         className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
       >
-        <NoteEditor />
+        <NoteEditor
+          type={openNoteEditorModal.type}
+          noteData={openNoteEditorModal.data}
+          onClose={() => {
+            setOpenNoteEditorModal({ isShown: false, type: 'add', data: null });
+          }}
+        />
       </Modal>
     </>
   );
