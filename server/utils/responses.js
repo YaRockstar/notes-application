bdcf51733f36f117fee72dbcd26474b58303ee14
Yaroslav Kalashnikov <yekalashnikov@gmail.com>
2024-10-08 20:53:16 +0300

    refactor: remove unnecessaru file

--- server/utils/responses.js
+++
@@ -1,8 +0,0 @@
-import RequestError from '../constants/errors.js';
-
-export const internalServerResponse = res => {
-  return res.status(RequestError.INTERNAL_SERVER_ERROR.status).json({
-    error: true,
-    message: RequestError.INTERNAL_SERVER_ERROR.message,
-  });
-};
