bdcd0c4071ee1bfac90692bdc7a4957df0e9ef45
Yaroslav Kalashnikov <yekalashnikov@gmail.com>
2024-12-01 15:03:50 +0300

    refactor: changing server port

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import { connectDb } from './db.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
